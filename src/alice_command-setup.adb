af0e8999e2093955eab4efc3eb252b70ffe7818d
Francesc Rocher <francesc.rocher@gmail.com>
2023-04-16 00:03:50 +0200

    wip: setup

--- src/alice_command-setup.adb
+++ src/alice_command-setup.adb
@@ -19,23 +19,28 @@ package body Alice_Command.Setup is
    -----------------
 
    procedure Setup_Alice is
-      FD           : GNAT.OS_Lib.File_Descriptor;
-      Status       : Integer;
+      FD     : GNAT.OS_Lib.File_Descriptor;
+      Status : Integer;
+
       Str_Restore  : aliased String := "restore";
       Str_CWD      : aliased String := ".";
       Str_Checkout : aliased String := "checkout";
       Str_Main     : aliased String := "main";
    begin
+      pragma Unreferenced (FD);
+
       Log.Info ("Setup Alice: first time execution");
       FD := GNAT.OS_Lib.Create_File (".setup", GNAT.OS_Lib.Text);
 
-      -- make sure any modified file is restored
+      --  make sure any modified file is restored
+      Log.Detail ("restoring files modified by alr");
       Status :=
         GNAT.OS_Lib.Spawn
           ("git", [Str_Restore'Unchecked_Access, Str_CWD'Unchecked_Access]);
       Log.Debug ("git restore returns" & Status'Image);
 
-      -- switch to branch 'main'
+      --  switch to branch 'main'
+      Log.Detail ("checkout branch main");
       Status :=
         GNAT.OS_Lib.Spawn
           ("git", [Str_Checkout'Unchecked_Access, Str_Main'Unchecked_Access]);
@@ -49,7 +54,8 @@ package body Alice_Command.Setup is
    overriding procedure Execute
      (Cmd : in out Cmd_Type; Args : AAA.Strings.Vector)
    is
-      Exists_Setup_File : Boolean := GNAT.OS_Lib.Is_Readable_File (".setup");
+      Exists_Setup_File : constant Boolean :=
+        GNAT.OS_Lib.Is_Readable_File (".setup");
    begin
       if not Exists_Setup_File then
          Setup_Alice;
