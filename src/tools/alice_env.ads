3368d193b13831dfdcec63ea9d9ccb44ddf880d0
Francesc Rocher <francesc.rocher@gmail.com>
2023-07-15 11:03:03 +0200

    Add file header

--- src/tools/alice_env.ads
+++ src/tools/alice_env.ads
@@ -1,3 +1,11 @@
+-------------------------------------------------------------------------------
+--
+--  ALICE - Adventures for Learning and Inspiring Coding Excellence
+--  Copyright (c) 2023 Francesc Rocher <francesc.rocher@gmail.com>
+--  SPDX-License-Identifier: MIT
+--
+-------------------------------------------------------------------------------
+
 package Alice_Env is
 
    function Is_Alice_Repository
