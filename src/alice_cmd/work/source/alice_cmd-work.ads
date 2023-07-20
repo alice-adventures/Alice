b6ef12f9862f0c318b2e1ff8fbc5d9d5f1211dbe
Francesc Rocher <francesc.rocher@gmail.com>
2023-07-20 22:39:52 +0200

    Add repository ro Source_Type

--- src/alice_cmd/work/source/alice_cmd-work.ads
+++ src/alice_cmd/work/source/alice_cmd-work.ads
@@ -14,11 +14,12 @@ package Alice_Cmd.Work is
    type Source_Name is (Project_Euler);
 
    type Source_Type is record
-      Name  : Unbounded_String;
-      Id    : Unbounded_String;
-      Tag   : Unbounded_String;
-      URL   : Unbounded_String;
-      About : AAA.Strings.Vector;
+      Name       : Unbounded_String;
+      Id         : Unbounded_String;
+      Tag        : Unbounded_String;
+      URL        : Unbounded_String;
+      Repository : Unbounded_String;
+      About      : AAA.Strings.Vector;
    end record;
 
    --!pp off
@@ -26,11 +27,12 @@ package Alice_Cmd.Work is
 
    Available_Sources : constant array (Source_Name) of Source_Type :=
      [Project_Euler =>
-       (Name  => To_Unbounded_String ("Project Euler"),
-        Id    => To_Unbounded_String ("project_euler"),
-        Tag   => To_Unbounded_String ("per"),
-        URL   => To_Unbounded_String ("https://projecteuler.net"),
-        About =>
+       (Name       => To_Unbounded_String ("Project Euler"),
+        Id         => To_Unbounded_String ("project_euler"),
+        Tag        => To_Unbounded_String ("per"),
+        URL        => To_Unbounded_String ("https://projecteuler.net"),
+        Repository => To_Unbounded_String ("alice-adventures/project_euler"),
+        About      =>
           AAA.Strings.Empty_Vector
           .Append ("Project Euler is a series of challenging mathematical/computer programming problems that will require more than just mathematical insights to solve.")
           .Append ("Although mathematics will help you arrive at elegant and efficient methods, the use of a computer and programming skills will be required to solve most problems."))];
