bb8ec21713ed64960b55a77008a75e7e4d413da6
Francesc Rocher <francesc.rocher@gmail.com>
2025-06-08 08:43:44 +0200

    Minimum initial test structure

---
+++ tests/source/unit/alice_unit_test.adb
@@ -0,0 +1,14 @@
+-------------------------------------------------------------------------------
+--
+--  ALICE - Adventures for Learning and Inspiring Coding Excellence
+--  Copyright (c) 2023-2025 Francesc Rocher <francesc.rocher@gmail.com>
+--  SPDX-License-Identifier: MIT
+--
+-------------------------------------------------------------------------------
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Alice_Unit_Test is
+begin
+   Put_Line ("Welcome to the Alice Test CLI");
+end Alice_Unit_Test;
